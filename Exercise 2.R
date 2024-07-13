ba7f503b3cd706d36b825344e3b1eb4aee8f238c
GbadamosiMuibat <81063006+GbadamosiMuibat@users.noreply.github.com>
2024-07-13 14:12:35 -0700

    Geo-reverse

---
+++ Exercise 2.R
@@ -0,0 +1,7 @@
+#Loading libraries
+library(tidyverse)
+library(tidygeocoder)
+library(sf)
+library(mapview)
+#read data
+Stanbic_Ibtc_Branches <- read.csv("C:/Users/user/Downloads/Stanbic_Ibtc_Branches.csv")
